a7d012fc7d1c04a6f91f5f166ffd259655f3fdb3
rohitrajverma <rohitrajverma1611@gmail.com>
2020-09-13 14:08:32 +0530

    Updates

---
+++ precache-manifest.070f013e34070af83aae56014fb9ce5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f1bbb4e0f2738aed126f677c8745c8",
+    "url": "/my-react-app/index.html"
+  },
+  {
+    "revision": "c4711d471b15cde379b5",
+    "url": "/my-react-app/static/css/main.7ca47df3.chunk.css"
+  },
+  {
+    "revision": "04f3c9bf39afaffed69d",
+    "url": "/my-react-app/static/js/2.6fe888fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-react-app/static/js/2.6fe888fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4711d471b15cde379b5",
+    "url": "/my-react-app/static/js/main.148e2c30.chunk.js"
+  },
+  {
+    "revision": "435064591b6543e8ca17",
+    "url": "/my-react-app/static/js/runtime-main.c0584337.js"
+  }
+]);
